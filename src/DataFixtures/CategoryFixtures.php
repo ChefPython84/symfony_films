2db77ef524ece2f1909b2c06c4eddb148053318d
ChefPython84 <erwan.couturier@etudiant.univ-reims.fr>
2024-01-29 17:17:14 +0100

    feat(data-fixtures): modification de CategoryFixtures pour avoir 10 catégories

--- src/DataFixtures/CategoryFixtures.php
+++ src/DataFixtures/CategoryFixtures.php
@@ -5,18 +5,19 @@ namespace App\DataFixtures;
 use App\Entity\Category;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-
+use Faker;
 class CategoryFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
         //Genere moi 5 objets de type Category fictifs
 
-        $names_category = ['Action', 'Aventure', 'Horreur', 'Comédie', 'Drame'];
-
-        foreach (range(1, 5) as $i) {
+        //$names_category = ['Action', 'Aventure', 'Horreur', 'Comédie', 'Drame'];
+        $faker = \Faker\Factory::create();
+        $faker->addProvider(new \Xylis\FakerCinema\Provider\Movie($faker));
+        foreach (range(1, 10) as $i) {
             $category = new Category();
-            $category->setName($names_category[$i - 1]);
+            $category->setName($faker->movieGenre());
             $manager->persist($category);
             $this->addReference("category_$i", $category); //On ajoute une référence à chaque objet généré
             // pour pouvoir les récupérer dans d'autres fixtures
