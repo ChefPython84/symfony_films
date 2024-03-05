667cc3ae0a469ca701d85a578713a8512c9f5ce0
ChefPython84 <erwan.couturier@etudiant.univ-reims.fr>
2024-03-05 16:16:19 +0100

    fix(actor.php): modification du serializer pour le mot de passe

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -10,6 +10,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 #[ApiResource]
 #[ORM\Entity(repositoryClass: UserRepository::class)]
@@ -22,20 +23,17 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     #[ORM\Column(length: 255)]
     #[Assert\NotBlank]
-    #[Assert\Length(min: 5, max: 255)]
     #[Groups(['user:read', 'user:write'])]
     private ?string $username = null;
 
     #[ORM\Column(length: 255)]
     #[Assert\NotBlank]
-    #[Assert\Length(min: 5, max: 255)]
     #[Assert\Email]
     #[Groups(['user:write', 'user:read'])]
     private ?string $email = null;
 
     #[ORM\Column(length: 255)]
-    #[Assert\NotBlank]
-    #[Assert\Length(min: 5, max: 255)]
+
     #[Groups(['user:write', 'user:read'])]
     #[Assert\Regex(
         pattern: '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{6,}$/',
@@ -94,8 +92,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function getRoles(): array
     {
         $roles = $this->roles;
-        // guarantee every user at least has ROLE_USER
-        $roles[] = 'ROLE_USER';
         return array_unique($roles);
     }
     public function setRoles(array $roles): static
@@ -103,6 +99,15 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         $this->roles = $roles;
 
         return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+
+    public function eraseCredentials()
+    {
+        return null;
     }
 
     /**
@@ -120,15 +125,5 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
-
-    /**
-     * @see UserInterface
-     */
-
-    public function eraseCredentials()
-    {
-        return null;
-    }
-
 
 }
